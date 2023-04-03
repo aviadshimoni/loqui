bd0aa3a6d90f22ca1c99fd4880f3ad5b9e3bbed4
aviv <avivshemesh8@gmail.com>
2023-04-03 13:10:54 +0300

    started working on data preprocessing for poc

---
+++ scripts/prepare_lrw.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+import time
+from torch.utils.data import DataLoader
+from dataset.lrw_dataset_preparation import LRWDatasetPreparation
+
+
+def main():
+    dataset = LRWDatasetPreparation()
+    loader = DataLoader(dataset,
+                        batch_size=96,
+                        num_workers=16,
+                        shuffle=False)
+
+    start = time.time()
+
+    for i, batch in enumerate(loader):
+        end = time.time()
+        eta = ((end - start) / (i + 1) * (len(loader) - i)) / 3600.0
+        print(f"eta:{eta:.5f}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/utils.py
@@ -0,0 +1,64 @@
+# encoding: utf-8
+import os
+import cv2
+import numpy as np
+import system_globals as sg
+
+
+def ensure_dir(directory: str) -> None:
+    """
+    gets a path to directory, if it doesn't exist - create it
+    :param directory: path to directory
+    :return: None
+    """
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def extract_opencv(file_name: str) -> list:
+    """
+    gets a path to video file, try to extract the ROI from it
+    :param file_name: path to video file
+    :return: ROI of given video file
+    """
+
+    video = []
+    cap = cv2.VideoCapture(file_name)
+
+    while cap.isOpened():
+        ret, frame = cap.read()  # BGR
+
+        if ret:
+            frame = frame[115:211, 79:175]
+            frame = sg.jpeg.encode(frame)
+            video.append(frame)
+        else:
+            break
+
+    cap.release()
+
+    return video
+
+
+def load_duration(file: str) -> np.array:
+    """
+    gets a path to an video example file and return its duration calculated by numpy
+    :param file: path for video file
+    :return: duration of the file
+    """
+
+    with open(file, "r") as f:
+        lines = f.readlines()
+
+        for line in lines:
+            if line.find("Duration") != -1:
+                duration = float(line.split(' ')[1])
+
+    tensor = np.zeros(29)
+    mid = 29 / 2
+    start = int(mid - duration / 2 * 25)
+    end = int(mid + duration / 2 * 25)
+    tensor[start:end] = 1.0
+
+    return tensor
