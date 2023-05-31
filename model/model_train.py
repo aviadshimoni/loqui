e6a3f26fd127211b7e509672071f85975ba2f374
aviad_dv <aviad.shimoni@doubleverify.com>
2023-05-31 22:42:20 +0300

    comment torch backends

--- model/model_train.py
+++ model/model_train.py
@@ -11,7 +11,7 @@ from utils.helpers import get_logger
 
 logger = get_logger(__name__)
 
-torch.backends.cudnn.benchmark = True
+# torch.backends.cudnn.benchmark = True
 
 
 def train(lr: float, batch_size: int, n_class: int, max_epoch: int, num_workers: int = 1, gpus: str = '0',
