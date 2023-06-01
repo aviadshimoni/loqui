d2c48d9a132ff680d8c16ece62b9a1e756abdd1a
aviad_dv <aviad.shimoni@doubleverify.com>
2023-06-01 17:32:05 +0300

    new prepare vamos

--- scripts/prepare_lrw.py
+++ scripts/prepare_lrw.py
@@ -1,4 +1,3 @@
-# encoding: utf-8
 import os
 import cv2
 import glob
@@ -11,32 +10,17 @@ from moviepy.editor import VideoFileClip
 
 jpeg = TurboJPEG()
 
-
-def get_video_duration(file_path):
+def get_original_video_duration(file_path):
     video = VideoFileClip(file_path)
     duration = video.duration
     video.close()
     return duration
 
-
 def ensure_dir(directory: str) -> None:
-    """
-    gets a path to directory, if it doesn't exist - create it
-    :param directory: path to directory
-    :return: None
-    """
-
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-
 def load_duration(file: str) -> np.array:
-    """
-    gets a path to a video example file and returns its duration calculated by numpy
-    :param file: path for the video file
-    :return: duration of the file
-    """
-
     with open(file, 'r') as f:
         lines = f.readlines()
 
@@ -52,14 +36,7 @@ def load_duration(file: str) -> np.array:
 
     return tensor.astype(np.bool_)
 
-
-def extract_opencv(file_name: str) -> list:
-    """
-     Gets a path to a video file, tries to extract the ROI from it.
-     :param file_name: Path to the video file.
-     :return: ROI frames of the given video file.
-     """
-
+def extract_opencv(file_name: str, roi_coordinates: tuple) -> list:
     video = []
     cap = cv2.VideoCapture(file_name)
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -72,7 +49,7 @@ def extract_opencv(file_name: str) -> list:
         ret, frame = cap.read()
         if ret:
             frame = cv2.resize(frame, (256, 256))
-            frame = frame[115:211, 79:175]
+            frame = frame[roi_coordinates[0]:roi_coordinates[1], roi_coordinates[2]:roi_coordinates[3]]
             _, jpeg_frame = cv2.imencode('.jpg', frame)
             frame_bytes = jpeg_frame.tobytes()
             video.append(frame_bytes)
@@ -88,26 +65,17 @@ def extract_opencv(file_name: str) -> list:
 
     return video
 
-
-# target_dir = 'lrw_roi_80_116_175_211_npy_gray_pkl_jpeg'
 target_dir = '/tf/loqui/custom_lipread_pkls'
 ensure_dir(target_dir)
 
-
 class LRWDataset(Dataset):
-    """
-    Object that represents the preparation process of LRW dataset.
-    Inherits LRWDatasetInterface which implements Dataset of torch module
-    and generates training samples of LRW
-    """
-
-    def __init__(self, mp4_path='lrw_mp4'):
+    def __init__(self, mp4_path='lrw_mp4', roi_coordinates=(115, 211, 79, 175)):
         self.mp4_path = mp4_path
 
         with open('/tf/loqui/label_sorted.txt') as myfile:
             self.labels = myfile.read().splitlines()
 
-        self.list = []
+        self.data = []
 
         for i, label in enumerate(self.labels):
             files = glob.glob(os.path.join(self.mp4_path, label, '*', '*.mp4'))
@@ -118,50 +86,46 @@ class LRWDataset(Dataset):
                 ensure_dir(save_path)
 
             files = sorted(files)
-            self.list += [(file, i) for file in files]
+            self.data += [(file, i) for file in files]
+
+        self.roi_coordinates = roi_coordinates
 
     def __getitem__(self, idx: int) -> dict:
-        """
-        implements the operator []
-        :param idx: index to return of the dataset object
-        :return: by given index, return the respectively data on that index
-        """
+        video_path, label = self.data[idx]
+        inputs = extract_opencv(video_path, roi_coordinates=self.roi_coordinates)
 
-        inputs = extract_opencv(self.list[idx][0])
-
-        duration = self.list[idx][0]
-        metadata_file = duration.replace('.mp4', '.txt')
-        labels = self.list[idx][1]
+        metadata_file = video_path.replace('.mp4', '.txt')
 
         if os.path.isfile(metadata_file):
             duration = load_duration(metadata_file)
+            print(f"duration lrw mp4: {duration}")
         else:
-            duration = get_video_duration(self.list[idx][0])
+            original_duration = get_original_video_duration(video_path)
+            duration = original_duration * 29 / frame_count
+            print(f"duration loqui-custom mp4: {duration}")
 
-        result = {'video': inputs,
-                  'label': int(labels),
-                  'duration': duration}
+        result = {
+            'video': inputs,
+            'label': int(label),
+            'duration': duration
+        }
 
-        output = self.list[idx][0].replace(self.mp4_path, target_dir).replace('.mp4', '.pkl')
+        output = video_path.replace(self.mp4_path, target_dir).replace('.mp4', '.pkl')
         torch.save(result, output)
 
         return result
 
     def __len__(self) -> int:
-        """
-        implements the len operator
-        :return: len of self.list
-        """
-
-        return len(self.list)
-
+        return len(self.data)
 
 def main():
-    loader = DataLoader(LRWDataset("/tf/Daniel/custom_lipread_mp4"),
-                        batch_size=1,
-                        num_workers=16,
-                        shuffle=False,
-                        drop_last=False)
+    loader = DataLoader(
+        LRWDataset("/tf/Daniel/custom_lipread_mp4"),
+        batch_size=1,
+        num_workers=16,
+        shuffle=False,
+        drop_last=False
+    )
 
     tic = time.time()
 
@@ -169,7 +133,6 @@ def main():
         toc = time.time()
         eta = ((toc - tic) / (i + 1) * (len(loader) - i)) / 3600.0
         print(f'eta:{eta:.5f}')
-
 
 if __name__ == '__main__':
     main()
